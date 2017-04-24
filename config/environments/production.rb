ebb7ab2b65b163c3161f8ecf6abb010559bf119c
shanehuang <borderun1010@gmail.com>
2017-04-24 22:58:35 +0800

    made index beautiful

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,6 @@ Rails.application.configure do
     # Use a different logger for distributed setups.
     # require 'syslog/logger'
     # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-    config.serve_static_assets = true
-    config.assets.compile = true
 
     if ENV['RAILS_LOG_TO_STDOUT'].present?
         logger = ActiveSupport::Logger.new(STDOUT)
